fe4e58fd8bf3746cc1024bb19cd3fb7dfefa29f5
mrMarsik <petroimoroz@gmail.com>
2026-08-11 16:26:37 +0200

    i'm Neko

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import * as commands from "./commands";
 
 process.stdin.on("data", (data) => {
   const text = data.toString().trim() as keyof typeof commands;
-  try {
-  commands[text]()
-  } catch {}
+  const command = commands[text];
+  
+  if (command) {
+    command();
+  }
 });
 

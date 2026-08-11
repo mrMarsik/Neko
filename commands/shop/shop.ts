99139e89ade37ee5ad5f53cad679500c38dbecfc
mrMarsik <petroimoroz@gmail.com>
2026-08-11 23:00:32 +0200

    shop

--- commands/shop/shop.ts
+++ commands/shop/shop.ts
@@ -1,24 +1,19 @@
 import { txtCustom } from "../../txt-custom/txt-custom";
 
 
-export function shop() {
+
+
+function shop(data: Buffer) {
+  const text = data.toString().trim();
+  
+  if (text === "exit") {
+    console.log(txtCustom("ok"))
+    process.stdin.off("data", shop);
+    return;
+  }
   
   const test = "Welcome to the shop! Here you can buy items and upgrades for your character."
   console.log(txtCustom(test))
-  
-  
-  process.stdin.on("data", (data) => {
-  const text = data.toString().trim()
-  
-
-  if (text === "exit") {
-
-    console.log(txtCustom("ok"))
-    process.exit()
-
-  } else {
-
-    console.log(txtCustom(text))}
-
-  })
 }
+
+process.stdin.on("data", shop);
